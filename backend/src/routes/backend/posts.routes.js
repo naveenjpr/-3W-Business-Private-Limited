c8e3d8194330e0b1f73ebe774a50b8ac0bf6555a
naveenjpr <naveensainijpr@gmail.com>
2026-02-07 11:42:43 +0530

    djeuu

--- backend/src/routes/backend/posts.routes.js
+++ backend/src/routes/backend/posts.routes.js
@@ -2,17 +2,13 @@ const express = require("express");
 const route = express.Router();
 const PostController = require("../../controllers/backend/posts.controller");
 
-// ✅ Cloudinary upload middleware
-const upload = require("../../config/upload").default;
+const upload = require("../../config/upload");
 
 module.exports = (app) => {
-  // CREATE POST (with image)
   route.post("/add", upload.single("image"), PostController.create);
 
-  // VIEW POSTS
   route.post("/view", PostController.view);
 
-  // LIKE / UNLIKE POST
   route.post("/like", PostController.likePost);
 
   app.use("/api/backend/post", route);
